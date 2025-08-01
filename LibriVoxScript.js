9791074f997f97b22f39b08d252ca94206bde0c1
Stefan <84-stefancruz@users.noreply.gitlab.futo.org>
2025-08-01 01:31:02 +0100

    fix: disable cache source for now

--- LibriVoxScript.js
+++ LibriVoxScript.js
@@ -295,17 +295,6 @@ source.getContentDetails = function (url) {
     const duration = chapter.duration;
 
     const sources = [];
-   
-    if(chapter.section_id) {
-        sources.push(new AudioUrlSource({
-            name: 'audio (cached v2)',
-            container: 'audio/mpeg',
-            codec: 'mp4a.40.2',
-            url: `https://librivox-api.openaudiobooks.org/api/v2/proxy/${chapter.section_id}.mp3`,
-            language: 'Unknown',
-            duration,
-        }));
-    }
 
     if (chapter.chapterFile) {
         sources.push(new AudioUrlSource({
@@ -316,6 +305,10 @@ source.getContentDetails = function (url) {
             language: 'Unknown',
             duration
         }));
+    }
+
+    if (sources.length === 0) {
+        throw new ScriptException(`No audio sources found for chapter`);
     }
 
     return new PlatformVideoDetails({
